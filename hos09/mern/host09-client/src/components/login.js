2cb9b31d62e04227dfb1d12acecfefb33d7c3231
Tyler <50530351+adams100@users.noreply.github.com>
2023-09-09 00:41:31 +0000

    HOS10 - Tyler Adams

---
+++ hos09/mern/host09-client/src/components/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = () => {
+    return ( 
+        <a href="https://shiny-space-disco-5x7w4pwqjgrh449x-5050.app.github.dev/facebook"
+        className="btn btn-primary">Login with Facebook</a>
+    )
+}
+
+export default Login;
